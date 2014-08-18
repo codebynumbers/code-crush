d4069fffb79bd10ef5c290498277066163101cf0
Rob Harrigan <harrigan.rob@gmail.com>
2014-08-18 10:54:06 -0400

    Path fix

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-$HOME/venv/bin/gunicorn -k flask_sockets.worker -t 5000 --bind 0.0.0.0:80 server:app
+$HOME/code-crush/venv/bin/gunicorn -k flask_sockets.worker -t 5000 --bind 0.0.0.0:80 server:app

547fb38770cfe7c336b47f3db935372cc6d9f4dd
Rob Harrigan <harrigan.rob@gmail.com>
2014-08-18 11:08:58 -0400

    Update startup script

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-$HOME/code-crush/venv/bin/gunicorn -k flask_sockets.worker -t 5000 --bind 0.0.0.0:80 server:app
+cd $HOME/code-crush
+venv/bin/gunicorn -k flask_sockets.worker -t 5000 --bind 0.0.0.0:80 server:app

030fab6706406b1c90285be29ee67a1a4d448e10
Rob Harrigan <harrigan.rob@gmail.com>
2014-08-21 17:56:14 -0400

    Move backend back for now

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ import shutil
 from flask import Flask, render_template
 from flask_sockets import Sockets
 from random import randint
-from backend import Backend
+#from backend import Backend
 
 
 app = Flask(__name__)
@@ -31,6 +31,54 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 docker = Docker.Client(base_url='unix://var/run/docker.sock',
             version='1.9',
             timeout=5)
+
+class Backend(object):
+    """Interface for registering and updating WebSocket clients."""
+
+    def __init__(self):
+        # map of room => clients[]
+        self.room_clients = {}
+        # map of clients => room
+        self.client_room = {}
+
+        self.pubsub = redis.pubsub()
+        self.pubsub.subscribe(app.config['REDIS_CHAN'])
+
+
+    def __iter_data(self):
+        for message in self.pubsub.listen():
+            data = message.get('data')
+            if message['type'] == 'message':
+                #app.logger.info(u'Sending message: {}'.format(data))
+                yield data
+
+    def register(self, client, room):
+        """Register a WebSocket connection for Redis updates."""
+        if not self.room_clients.get(room):
+            self.room_clients[room] = []
+        self.room_clients[room].append(client)
+        self.client_room[client] = room
+
+    def send(self, client, data):
+        """Send given data to the registered client.
+        Automatically discards invalid connections."""
+        try:
+            client.send(data)
+        except Exception:
+            # find client's room and remove them from appropriate list
+            room = self.client_room[client]
+            self.room_clients[room].remove(client)
+
+    def run(self):
+        """Listens for new messages in Redis, and sends them to clients."""
+        for data in self.__iter_data():
+            room = json.loads(data)['room']
+            for client in self.room_clients.get(room, []):
+                gevent.spawn(self.send, client, data)
+
+    def start(self):
+        """Maintains Redis subscription in the background."""
+        gevent.spawn(self.run)
 
 editors = Backend()
 editors.start()

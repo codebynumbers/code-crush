698928485e09d767266d2acfded05a2cb15c9003
Rob Harrigan <harrigan.rob@gmail.com>
2014-08-24 21:01:08 -0400

    Remove redundant data structure

--- backend.py
+++ backend.py
@@ -8,12 +8,8 @@ class Backend(object):
     def __init__(self, redis, channel):
         # map of room => clients[]
         self.room_clients = {}
-        # map of clients => room
-        self.client_room = {}
-
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(channel)
-
 
     def __iter_data(self):
         for message in self.pubsub.listen():
@@ -24,19 +20,24 @@ class Backend(object):
 
     def register(self, client, room):
         """Register a WebSocket connection for Redis updates."""
+        """
+        room_clients = {
+            'default': [ws_client_0, ws_client_1, ...],
+            'misc': [ws_client_2, ws_client_3, ...]
+        } 
+        """
         if not self.room_clients.get(room):
             self.room_clients[room] = []
-        self.room_clients[room].append(client)
-        self.client_room[client] = room
+        if client not in self.room_clients[room]:
+            self.room_clients[room].append(client)
 
-    def send(self, client, data):
+    def send(self, client, room, data):
         """Send given data to the registered client.
         Automatically discards invalid connections."""
         try:
             client.send(data)
         except Exception:
             # find client's room and remove them from appropriate list
-            room = self.client_room[client]
             self.room_clients[room].remove(client)
 
     def run(self):
@@ -44,7 +45,7 @@ class Backend(object):
         for data in self.__iter_data():
             room = json.loads(data)['room']
             for client in self.room_clients.get(room, []):
-                gevent.spawn(self.send, client, data)
+                gevent.spawn(self.send, client, room, data)
 
     def start(self):
         """Maintains Redis subscription in the background."""

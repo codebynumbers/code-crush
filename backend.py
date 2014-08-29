aa2de92ddeddf3da593e3fff42fcc19244470f46
Rob Harrigan <harrigan.rob@gmail.com>
2014-08-28 21:55:20 -0400

    Fix client room list and language selection

--- backend.py
+++ backend.py
@@ -8,9 +8,6 @@ class Backend(object):
     def __init__(self, redis, channel):
         # map of room => clients[]
         self.room_clients = {}
-        # map of clients => room
-        self.client_room = {}
-
         self.pubsub = redis.pubsub()
         self.pubsub.subscribe(channel)
 
@@ -27,16 +24,14 @@ class Backend(object):
         if not self.room_clients.get(room):
             self.room_clients[room] = []
         self.room_clients[room].append(client)
-        self.client_room[client] = room
 
-    def send(self, client, data):
+    def send(self, client, room, data):
         """Send given data to the registered client.
         Automatically discards invalid connections."""
         try:
             client.send(data)
         except Exception:
             # find client's room and remove them from appropriate list
-            room = self.client_room[client]
             self.room_clients[room].remove(client)
 
     def run(self):
@@ -44,7 +39,7 @@ class Backend(object):
         for data in self.__iter_data():
             room = json.loads(data)['room']
             for client in self.room_clients.get(room, []):
-                gevent.spawn(self.send, client, data)
+                gevent.spawn(self.send, client, room, data)
 
     def start(self):
         """Maintains Redis subscription in the background."""
